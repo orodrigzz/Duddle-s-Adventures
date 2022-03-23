fca3938b141337c6c0d081399ee454b6b933b985
Darsirious <89274777+Darsirious@users.noreply.github.com>
2022-03-23 12:45:28 +0100

    slpash al disparar QUEDA HACER

--- Duddle's Adventures/Assets/Scripts/Projectile.cs
+++ Duddle's Adventures/Assets/Scripts/Projectile.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-
+    public GameObject Color1;
+    public GameObject Color2;
     public float speed;
     public float lifeTime;
     public float distance;
     public int damage;
+    int minval = 0;
+    int maxval = 2;
+    int randval;
     public LayerMask whatIsSolid;
+    public GameObject SpriteHit1;
+    public GameObject SpriteHit2;
+    public GameObject SpriteHit3;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        for (int i = minval; i< maxval; i++)
+        {
+            randval = Random.Range(0, 2);
+        }
         Invoke("DestroyProjectile", lifeTime);
     }
+
 
     // Update is called once per frame
     void Update()
@@ -31,11 +45,41 @@ public class Projectile : MonoBehaviour
             }
             DestroyProjectile();
 
+            if (randval == 0)
+            {
+                 Instantiate(SpriteHit1, transform.position, new Quaternion(0, 0, 0, 0));
+            }
+
+            if (randval == 1)
+            {
+
+                Instantiate(SpriteHit2, transform.position, new Quaternion(0, 0, 0, 0));
+            }
+
+            if (randval == 2)
+            {
+                Instantiate(SpriteHit3, transform.position, new Quaternion(0, 0, 0, 0));
+            }
+ 
+
             if (hitInfo.collider.CompareTag("Pigero") || hitInfo.collider.CompareTag("PigeroGreen"))
             {
                 //Debug.Log("Enemy must take dmg");
                 hitInfo.collider.GetComponent<pigero>().TakeDamage(damage);
                 DestroyProjectile();
+
+                if (randval == 0)
+                {
+                    Instantiate(SpriteHit1, transform.position, new Quaternion(0, 0, 0, 0));
+                }
+                if (randval == 1)
+                {
+                    Instantiate(SpriteHit2, transform.position, new Quaternion(0, 0, 0, 0));
+                }
+                if (randval == 2)
+                {
+                    Instantiate(SpriteHit3, transform.position, new Quaternion(0, 0, 0, 0));
+                }
             }
             DestroyProjectile();
         }
